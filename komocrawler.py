be775194e5e7c2d5cf48b63f022fa5c900ec8038
jameskomo <james.komoh@gmail.com>
2019-06-05 13:42:34 +0300

    For loop created to iterate through all sections and print funtion used to test output

--- komocrawler.py
+++ komocrawler.py
@@ -15,8 +15,18 @@ vid_src=article.find('iframe', class_='youtube-player')['src']
 vid_id=vid_src.split('/')[4]
 vid_id=vid_id.split('?')[0]
 yt_link=f'https://youtube.com/watch?v={vid_id}'
-print(yt_link)
+# print(yt_link)
 
+# ITERATING THROUGH ALL CONTENT
+for article in soup.find_all('article'):
+    headline=article.h2.a.text
+    summary=article.find('div', class_='entry-content').p.text
+    video_source=article.find('iframe', class_='youtube-player')
+    vid_src=article.find('iframe', class_='youtube-player')['src']
+    vid_id=vid_src.split('/')[4]
+    vid_id=vid_id.split('?')[0]
+    yt_link=f'https://youtube.com/watch?v={vid_id}'
+    print(yt_link)
 
 
 # SCRAPING HTML FILE
