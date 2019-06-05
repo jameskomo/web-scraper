e73d5bcb78272d2ca92e75a8d0d8b057e9a1b971
jameskomo <james.komoh@gmail.com>
2019-06-05 13:36:50 +0300

    Video source manipulation cerated, fetching by video source, splitting by special characters and index and concatenating with standard video source format for final render

--- komocrawler.py
+++ komocrawler.py
@@ -4,7 +4,18 @@ import requests
 # SCRAPING WEBSITES
 source=requests.get('http://coreyms.com').text
 soup=BeautifulSoup(source, 'lxml')
-print(soup.prettify())
+article=soup.find('article')
+headline=article.h2.a.text
+summary=article.find('div', class_="entry-content").p.text
+# print(summary)
+
+# GETTING VIDEOS
+video_source=article.find('iframe', class_='youtube-player')
+vid_src=article.find('iframe', class_='youtube-player')['src']
+vid_id=vid_src.split('/')[4]
+vid_id=vid_id.split('?')[0]
+yt_link=f'https://youtube.com/watch?v={vid_id}'
+print(yt_link)
 
 
 
